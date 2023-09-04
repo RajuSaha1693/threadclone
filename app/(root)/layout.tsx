8915fc08cf87ab086cc7befe9c38a80c890aac90
Raju Saha <rajusaha010693@gmail.com>
2023-09-04 21:46:06 +0530

    added basic files

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,10 @@ import { Inter } from "next/font/google";
 import React from "react";
 
 import "../globals.css";
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
 export const metadata = {
   title: "Threads Clone",
   desc: "Thread Clone using Next JS",
@@ -17,7 +21,17 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className}`}>{children}</body>
+        <body className={`${inter.className}`}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   );
