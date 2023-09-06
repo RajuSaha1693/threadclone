1f0315836b67a86f566835ca8c7f8b8b4c160f58
Raju Saha <rajusaha010693@gmail.com>
2023-09-06 08:17:41 +0530

    added topbar and sidebar structure

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,3 +1,12 @@
+import { sidebarLinks } from "@/constants/";
 export default function LeftSidebar() {
-  return <div>LeftSidebar</div>;
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map((link) => (
+          <div>Link</div>
+        ))}
+      </div>
+    </section>
+  );
 }
